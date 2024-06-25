c1c9e6f51cb16934089db60e5d125f67b9d1b255
CahFernandes <kmilinhamf88@gmail.com>
2024-06-24 21:51:07 -0300

    Atividade 05 - 24/06/2024

--- Atividade05/exercicio.03.js
+++ Atividade05/exercicio.03.js
@@ -8,10 +8,10 @@
 
 const ler = require('readline-sync');
 
-var counts = [];
-
-let livros = [];
-let check    = true;
+var counts       = [];
+let livros_categ = [];
+let livros       = [];
+let check        = true;
 
 while(check){
     let titulo = ler.question("Informe o titulo do livro: ");
@@ -26,10 +26,40 @@ while(check){
 
 // Informa a quantidade de livros a serem analisados
 let qtde = ler.questionInt("Informe a quantidade de livros a serem analisados: ");
-qtde_livros = livros.slice(0,qtde);
-console.table(qtde_livros);
+livros_analis = livros.slice(0,qtde);
+console.table(livros_analis);
 
-// Agrupar os livros por categoria
+// Ordena a matriz qtde_livros por categoria
+livros_analis.sort((a,b) => a.categoria.localeCompare(b.categoria));
+
+let qtde_livro = 0;
+let categ_ant  = "";
+for (let i = 0; i < livros_analis.length; i++) {
+    if(livros_analis[i]['categoria'] != categ_ant) {
+        qtde_livro = 1;        
+    } else {
+        qtde_livro++;        
+    }
+
+    // busca o indice já existente
+    let pos = livros_categ.indexOf(livros_analis[i]['categoria']);
+    console.log(pos, categ_ant);
+    if(pos >= 0) {
+        livros_categ[[pos]['qtde_livro']] = qtde_livro;
+    } else {    
+        livros_categ.push({categoria: livros_analis[i]['categoria'], qtde_livro: qtde_livro});
+    }
+    categ_ant = livros_analis[i]['categoria'];
+}
+
+for (let index = 0; index < livros_categ.length; index++) {
+    if(livros_categ[index]['qtde_livro'] > 1) {
+        console.log(`Na categoria ${livros_categ[index]['categoria']}, possuem ${livros_categ[index]['qtde_livro']} livros cadastrados.`);    
+    } else {
+        console.log(`Na categoria ${livros_categ[index]['categoria']}, possui ${livros_categ[index]['qtde_livro']} livros cadastrado.`);    
+    }    
+}
+
 
 
 
